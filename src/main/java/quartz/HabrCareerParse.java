11086d88a2d32cdb12f8986566c5f7ff1f4a4785
Mingrow <nikitosvozh@gmail.com>
2023-08-11 10:42:01 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/quartz/HabrCareerParse.java
+++ src/main/java/quartz/HabrCareerParse.java
@@ -16,18 +16,24 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element dateElement = row.select(".vacancy-card__date").first();
-            HabrCareerDateTimeParser timeParser = new HabrCareerDateTimeParser();
-            LocalDateTime dateTime = timeParser.parse(dateElement.child(0).attr("datetime"));
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s %s%n", vacancyName, link, dateTime);
-        });
+        parseToPage(5);
+    }
+
+    private static void parseToPage(int page) throws IOException {
+        for (int i = 1; i <= page; i++) {
+            Connection connection = Jsoup.connect(String.format(PAGE_LINK + "?page=%s", i));
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element dateElement = row.select(".vacancy-card__date").first();
+                HabrCareerDateTimeParser timeParser = new HabrCareerDateTimeParser();
+                LocalDateTime dateTime = timeParser.parse(dateElement.child(0).attr("datetime"));
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.printf("%s %s %s%n", vacancyName, link, dateTime);
+            });
+        }
     }
 }
