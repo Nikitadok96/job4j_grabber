fe27c13e580a8f207d5022b202c085b224ebe608
Mingrow <nikitosvozh@gmail.com>
2023-08-12 15:36:14 +0300

    7. Web. [#289478]

--- src/main/java/grabber/Grabber.java
+++ src/main/java/grabber/Grabber.java
@@ -4,7 +4,12 @@ import grabber.utils.HabrCareerDateTimeParser;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -17,6 +22,7 @@ public class Grabber implements Grab {
     private final Parse parse;
     private final Store store;
     private final Scheduler scheduler;
+    private static Properties cfg;
     private final int time;
     private static final int PAGE_COUNT = 5;
 
@@ -61,7 +67,7 @@ public class Grabber implements Grab {
     }
 
     public static void main(String[] args) throws Exception {
-        var cfg = new Properties();
+        cfg = new Properties();
         try (InputStream in = Grabber.class.getClassLoader()
                 .getResourceAsStream("app.properties")) {
             cfg.load(in);
@@ -71,7 +77,29 @@ public class Grabber implements Grab {
         var parse = new HabrCareerParse(new HabrCareerDateTimeParser());
         var store = new PsqlStore(cfg);
         var time = Integer.parseInt(cfg.getProperty("time"));
-        new Grabber(parse, store, scheduler, time).init();
+        Grabber grabber = new Grabber(parse, store, scheduler, time);
+        grabber.init();
+        grabber.web(store);
+    }
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
 
